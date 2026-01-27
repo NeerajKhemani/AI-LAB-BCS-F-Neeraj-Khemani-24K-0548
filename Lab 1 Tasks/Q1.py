f563a15aba43747ee7b0ee9c12a3f2f05d8e6a65
NeerajKhemani <neerajkhemani009@gmail.com>
2026-01-27 22:52:10 +0500

    Add grading system based on student marks

--- Lab 1 Tasks/Q1.py
+++ Lab 1 Tasks/Q1.py
@@ -1,1 +1,15 @@
+name = input("Enter student name: ")
+marks = int(input("Enter marks: "))
 
+if marks>= 80:
+    grade = 'A'
+elif marks>= 70:
+    grade = 'B'
+elif marks>= 50:
+    grade = 'C'        
+else:
+    grade = 'Fail'    
+
+print('Student Name:', name)    
+print('Marks:', marks)
+print('Grade:', grade)
